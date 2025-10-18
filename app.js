74bdd9f84aaead694ffe3804b2cd1490174be6c4
Vimalraj S <vimalraj.s@hepl.com>
2025-10-18 21:04:39 +0530

    CORS issue fixed

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import CommonRoutes from "./src/routes/commonRoute.js";
 import cors from 'cors';
+
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use("/qtrack", CommonRoutes);
-app.use(cors());
 app.get("/", (req, res) => {
   res.send("Backend server running sucessfully");
 });
