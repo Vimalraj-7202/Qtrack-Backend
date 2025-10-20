9bb88ac979eddc906790bdc7b5441b9689caad6d
Vimalraj S <vimalraj.s@hepl.com>
2025-10-20 20:32:01 +0530

    Login  issue fixed

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -8,7 +8,7 @@ export const verifyToken = async (req, res, next) => {
     if (!token) return res.status(401).json({ message: "No token provided" });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  console.log("🔑 Decoded token:", decoded);
+  console.log("Decoded token:", decoded);
 
     const user = await User.findById(decoded.id).select("-password");
     if (!user) return res.status(401).json({ message: "User not found" });
