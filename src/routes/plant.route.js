ac3669e7e9f409d173615ce6f21700c07aca71e8
Vimalraj S <vimalraj.s@hepl.com>
2026-02-12 22:06:34 +0530

    unwanted files cleaned

--- src/routes/plant.route.js
+++
@@ -1,7 +0,0 @@
-import {createPlant} from '../controllers/plant/plant.controller.js';
-import express from 'express';
-
-const router=express.Router();
-router.post('/createPlant',createPlant);
-
-export default router;
--- src/routes/report.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getAllReports } from "../controllers/report/report.controller.js";
-
-const router=express.Router();
-router.get('/overallReport',getAllReports);
-
-export default router;
